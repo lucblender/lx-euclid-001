8f28142922ebe79a9eb422640660f1666bafc5a1
lucblender <lucasbonvin@hotmail.com>
2023-08-29 10:44:16 +0200

    put everything in a class and implement incr and decr detection

--- demoMpr121.py
+++ demoMpr121.py
@@ -1,82 +1,146 @@
 from mpr121 import MPR121
 from machine import Pin, I2C
-import time
+import utime as time
 
-i2c = I2C(0, sda=Pin(0), scl=Pin(1))
-mpr = MPR121(i2c)
+class CapacitivesCircles():
+    MAX_DELAY_INCR_DECR_MS = 1000
+    STEP_TRIGGER_INCR_DEGREE = 5
 
-list_concordance_sensor = [ 5, 4, 3, 11 ,10, 9, 8, 7, 6, 2, 1, 0]
+    NO_INCR_DECR_EVENT = 0
+    INNER_CIRCLE_INCR_EVENT = 1
+    INNER_CIRCLE_DECR_EVENT = 2
+    OUTER_CIRCLE_INCR_EVENT = 3
+    OUTER_CIRCLE_DECR_EVENT = 4
+    
+    def __init__(self):
+        self.i2c = I2C(0, sda=Pin(0), scl=Pin(1))
+        self.mpr = MPR121(self.i2c)
 
+        # the circles are routed on the PCB for convenience and doesn't follow electrodes
+        # numbering, this list help to re-order everything
+        self.list_concordance_sensor = [ 5, 4, 3, 11 ,10, 9, 8, 7, 6, 2, 1, 0]
+
+        self.last_inner_circle_angle = 0
+        self.last_outer_circle_angle = 0
+
+        self.inner_circle_angle = 0
+        self.outer_circle_angle = 0
+
+        self.last_inner_circle_angle_timestamp_ms = time.ticks_ms()
+        self.last_outer_circle_angle_timestamp_ms = time.ticks_ms()
+
+
+
+    def get_touch_circles_updates(self):
+        datas = []
+        inner_circle_len = 0
+        outer_circle_len = 0
+        angle = 0
+        incr_decr_event = CapacitivesCircles.NO_INCR_DECR_EVENT
+        
+        inner_angle_updated = False
+        outer_angle_updated = False
+        for i in range(0,12):
+            data = self.mpr.filtered_data(i)
+            if data<150: 
+                if self.list_concordance_sensor[i]<6:
+                    inner_circle_len += 1
+                else:
+                    outer_circle_len += 1
+                datas.append((self.list_concordance_sensor[i],data))
+        datas = sorted(datas, key=lambda x: x[0]) 
+
+        #print(datas)        
+        if len(datas) > 1 and len(datas) < 4:
+            if inner_circle_len>outer_circle_len:
+                datas = [x for x in datas if x[0] < 6]
+                outer_circle_len = 0
+            else:            
+                datas = [x for x in datas if x[0] > 5]
+                inner_circle_len = 0
+        elif len(datas) != 1:   
+            datas = []
+        
+        if len(datas) == 2:
+            sensor_distance = abs(datas[0][0] - datas[1][0])
+            if sensor_distance != 1 and sensor_distance != 5:
+                datas = []
+
+        if len(datas)>0:
+            angle = 0
+            if inner_circle_len> 0:
+                index_factor_offset = 0
+            else:
+                index_factor_offset = 6
+                
+            if len(datas) == 1:
+                angle = (datas[0][0]-index_factor_offset)*60
+            else:
+                
+                indexes = [x[0] for x in datas]
+                if (0 in indexes and 5 in indexes) or (6 in indexes and 11 in indexes):
+                    
+                    data_first_sensor = datas[1][1]
+                    data_second_sensor = datas[0][1]
+                    index_factor = datas[1][0] - index_factor_offset
+                else:
+                    
+                    data_first_sensor = datas[0][1]
+                    data_second_sensor = datas[1][1]
+                    index_factor = datas[0][0] - index_factor_offset
+                    
+                """
+                #old angle computation, doesn't work well
+                factor = (data_first_sensor-50)/(110-50)
+                angle = index_factor*60 + factor*60
+                """    
+                #if 0 in indexes and 1 in indexes:
+                difference = data_first_sensor - data_second_sensor
+                factor = (difference+90)/180
+                angle = index_factor*60 + factor*60
+            if inner_circle_len> 0:
+                
+                if time.ticks_ms() - self.last_inner_circle_angle_timestamp_ms < CapacitivesCircles.MAX_DELAY_INCR_DECR_MS:
+                    if self.last_inner_circle_angle-angle > CapacitivesCircles.STEP_TRIGGER_INCR_DEGREE:               
+                        incr_decr_event = CapacitivesCircles.INNER_CIRCLE_DECR_EVENT
+                        self.last_inner_circle_angle = angle
+                    elif self.last_inner_circle_angle-angle < -CapacitivesCircles.STEP_TRIGGER_INCR_DEGREE:
+                        incr_decr_event = CapacitivesCircles.INNER_CIRCLE_INCR_EVENT
+                        self.last_inner_circle_angle = angle
+                else:
+                    self.last_inner_circle_angle = angle # do this to prevent incr-decr when we touch the sensor after long time
+                self.inner_circle_angle = angle
+                self.last_inner_circle_angle_timestamp_ms = time.ticks_ms()
+                
+                
+                #print("inner_angle ",angle, "angle_1", angle_1)
+                
+                inner_angle_updated = True
+            else:  
+                
+                 
+                if time.ticks_ms() - self.last_outer_circle_angle_timestamp_ms < CapacitivesCircles.MAX_DELAY_INCR_DECR_MS:
+                    if self.last_outer_circle_angle-angle > CapacitivesCircles.STEP_TRIGGER_INCR_DEGREE:
+                        incr_decr_event = CapacitivesCircles.OUTER_CIRCLE_DECR_EVENT
+                        self.last_outer_circle_angle = angle
+                    elif self.last_outer_circle_angle-angle < -CapacitivesCircles.STEP_TRIGGER_INCR_DEGREE:
+                        incr_decr_event = CapacitivesCircles.OUTER_CIRCLE_INCR_EVENT
+                        self.last_outer_circle_angle = angle
+                else:
+                    self.last_outer_circle_angle = angle # do this to prevent incr-decr when we touch the sensor after long time
+                self.outer_circle_angle = angle      
+                self.last_outer_circle_angle_timestamp_ms = time.ticks_ms()
+                
+                
+                #print("outer_angle ",angle, "angle_1", angle_1)
+                
+                outer_angle_updated = True
+
+        return inner_angle_updated, outer_angle_updated, incr_decr_event, angle
+
+capacitivesCircles = CapacitivesCircles()
 # check all keys
 while True:
-    to_print = ""
-    datas = []
-    inner_circle_len = 0
-    outer_circle_len = 0
-    for i in range(0,12):
-        data = mpr.filtered_data(i)
-        if data<150: 
-            if list_concordance_sensor[i]<6:
-                inner_circle_len += 1
-            else:
-                outer_circle_len += 1
-            datas.append((list_concordance_sensor[i],data))
-    datas = sorted(datas, key=lambda x: x[0]) 
-
-    #print(datas)        
-    if len(datas) > 1 and len(datas) < 4:
-        if inner_circle_len>outer_circle_len:
-            datas = [x for x in datas if x[0] < 6]
-            outer_circle_len = 0
-        else:            
-            datas = [x for x in datas if x[0] > 5]
-            inner_circle_len = 0
-    elif len(datas) != 1:   
-        datas = []
-    
-    if len(datas) == 2:
-        sensor_distance = abs(datas[0][0] - datas[1][0])
-        if sensor_distance != 1 and sensor_distance != 5:
-            datas = []
-
-    if len(datas)>0:
-        angle = 0
-        angle_1 = 0
-        if inner_circle_len> 0:
-            index_factor_offset = 0
-        else:
-            index_factor_offset = 6
-            
-        if len(datas) == 1:
-            angle = (datas[0][0]-index_factor_offset)*60
-            angle_1 = angle
-        else:
-            
-            indexes = [x[0] for x in datas]
-            if (0 in indexes and 5 in indexes) or (6 in indexes and 11 in indexes):
-                
-                data_first_sensor = datas[1][1]
-                data_second_sensor = datas[0][1]
-                index_factor = datas[1][0] - index_factor_offset
-            else:
-                
-                data_first_sensor = datas[0][1]
-                data_second_sensor = datas[1][1]
-                index_factor = datas[0][0] - index_factor_offset
-                
-                
-            factor = (data_first_sensor-50)/(110-50)
-            angle = index_factor*60 + factor*60
-                
-            #if 0 in indexes and 1 in indexes:
-            difference = data_first_sensor - data_second_sensor
-            factor = (difference+90)/180
-            angle_1 = index_factor*60 + factor*60
-        if inner_circle_len> 0:
-            print("inner_angle ",angle, "angle_1", angle_1)
-        else:  
-            print("outer_angle ",angle, "angle_1", angle_1)
-
-        
+    print(capacitivesCircles.get_touch_circles_updates())   
         
     time.sleep_ms(50)
