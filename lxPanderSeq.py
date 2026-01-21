4311aa4bd2d094db1baf1a7e5980e57ecf1f4063
Lucas Bonvin <lucasbonvin@hotmail.com>
2026-01-21 17:52:35 +0100

    feature: implement getter and setter on lxPanderSeq with temporary tests

--- lxPanderSeq.py
+++ lxPanderSeq.py
@@ -2,9 +2,22 @@ from micropython import const
 import ustruct
 
 LX_PANDER_SEQ_I2C_ADDRESS = const(0x17)
-LX_PANDER_SEQ_MAJOR = const(0x00)
-LX_PANDER_SEQ_MINOR = const(0x01)
-LX_PANDER_SEQ_FIX = const(0x02)
+
+
+class MemoryAddress():
+    MAJOR = const(0x00)
+    MINOR = const(0x01)
+    FIX = const(0x02)
+    HAS_CHANGE = const(0x03)
+    FOCUS_RHYTHM = const(0x04)
+    RHYTHM0_LSB = const(0x05)
+    RHYTHM0_MSB = const(0x06)
+    RHYTHM1_LSB = const(0x07)
+    RHYTHM1_MSB = const(0x08)
+    RHYTHM2_LSB = const(0x09)
+    RHYTHM2_MSB = const(0x0A)
+    RHYTHM3_LSB = const(0x0B)
+    RHYTHM3_MSB = const(0x0C)
 
 
 class LxPanderSeq:
@@ -19,13 +32,36 @@ class LxPanderSeq:
             self.connected = False
 
         if self.connected:
-            self.major = self._register8(LX_PANDER_SEQ_MAJOR)
-            self.minor = self._register8(LX_PANDER_SEQ_MINOR)
-            self.fix = self._register8(LX_PANDER_SEQ_FIX)
+            self.major = self._register8(MemoryAddress.MAJOR)
+            self.minor = self._register8(MemoryAddress.MINOR)
+            self.fix = self._register8(MemoryAddress.FIX)
         else:
             self.major = 0
             self.minor = 0
             self.fix = 0
+
+    def get_has_change(self):
+        return self._register8(MemoryAddress.HAS_CHANGE)
+
+    def get_focus_rhythm(self):
+        return self._register8(MemoryAddress.FOCUS_RHYTHM)
+
+    def set_focus_rhythm(self, value):
+        self._register8(MemoryAddress.FOCUS_RHYTHM, value)
+
+    def set_rhythm(self, index, rhythm_array):
+        rhythm = 0
+        for i in range(16):
+            rhythm |= rhythm_array[i] << i
+
+        self._register16(MemoryAddress.RHYTHM0_LSB + index * 2, rhythm)
+
+    def get_rhythm(self, index):
+        rhythm = self._register16(MemoryAddress.RHYTHM0_LSB + index * 2)
+        result = []
+        for i in range(16):
+            result.append((rhythm >> i) & 0x01)
+        return result
 
     def _register8(self, register, value=None):
         if value is None:
@@ -40,3 +76,47 @@ class LxPanderSeq:
 
     def get_version_string(self):
         return f"v{self.major}.{self.minor}.{self.fix}"
+
+
+if __name__ == "__main__":
+    from machine import Pin, I2C
+    from utime import sleep
+
+    i2c = I2C(1, sda=Pin(26), scl=Pin(27), freq=1_000_000)
+    lx_pander_seq = LxPanderSeq(i2c)
+    print("LxPanderSeq connected:", lx_pander_seq.connected)
+    print("LxPanderSeq version:", lx_pander_seq.get_version_string())
+
+    rhythm0_before = lx_pander_seq.get_rhythm(0)
+    print("Rhythm0 before:", rhythm0_before)
+
+    lx_pander_seq.set_rhythm(0, [1]*16)
+
+    rhythm0_after = lx_pander_seq.get_rhythm(0)
+    print("Rhythm0 after set:", rhythm0_after)
+
+    focus_rhythm_before = lx_pander_seq.get_focus_rhythm()
+    print("Focus Rhythm before:", focus_rhythm_before)
+
+    lx_pander_seq.set_focus_rhythm(0x00)
+
+    focus_rhythm_after = lx_pander_seq.get_focus_rhythm()
+    print("Focus Rhythm after:", focus_rhythm_after)
+
+    has_change_before = lx_pander_seq.get_has_change()
+    print("Has Change before:", has_change_before)
+
+    print("Waiting 5 seconds to update buttons...")
+    for i in range(5, 0, -1):
+        print(i)
+        sleep(1)
+
+    has_change_after = lx_pander_seq.get_has_change()
+    print("Has Change after:", has_change_after)
+
+    rhythm0_after = lx_pander_seq.get_rhythm(0)
+    print("Rhythm0 after change:", rhythm0_after)
+
+    sleep(1)
+    has_change_after = lx_pander_seq.get_has_change()
+    print("Has Change after read:", has_change_after)
